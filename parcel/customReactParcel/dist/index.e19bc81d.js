1fbee1a6de9148a2226497e1b83ba33c63b1e7c8
Ankit yadav <118676491+ankit5646@users.noreply.github.com>
2024-02-28 00:01:26 +0530

    parcel

---
+++ parcel/customReactParcel/dist/index.e19bc81d.js
@@ -0,0 +1,19 @@
+function customRender(a, b) {
+    const domElement = document.createElement(a.type);
+    domElement.innerHTML = a.child;
+    domElement.setAttribute("href", a.props.href);
+    domElement.setAttribute("target", a.props.target);
+    b.appendChild(domElement);
+}
+const reactElement = {
+    type: "a",
+    props: {
+        href: "https://google.com",
+        target: "blank"
+    },
+    child: "click me bruhh!!"
+};
+const mainContainer = document.getElementById("root");
+customRender(reactElement, mainContainer);
+
+//# sourceMappingURL=index.e19bc81d.js.map
